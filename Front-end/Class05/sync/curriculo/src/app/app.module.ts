8646f961cc90ecc7a9f2b9ff4cfc05e9f5afb98a
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-11-11 20:22:25 -0300

    Class 05 - Angular: Components [sync class]

--- Front-end/Class05/sync/curriculo/src/app/app.module.ts
+++ Front-end/Class05/sync/curriculo/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
