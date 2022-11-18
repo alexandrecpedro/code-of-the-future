22b674a30093f37503f1cfbbe86bce60fab14367
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-11-18 19:14:49 -0300

    Class 09 - Angular: Routes and Modules [sync class]

---
+++ Front-end/Class09/sync/curriculo/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { registerLocaleData } from '@angular/common';
+import ptBr from "@angular/common/locales/pt";
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { CpfFormatPipe } from './pipes/cpf-format/cpf-format.pipe';
+import { PhoneFormatPipe } from './pipes/phone-format/phone-format.pipe';
+
+registerLocaleData(ptBr);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PhoneFormatPipe,
+    CpfFormatPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: "pt" },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: "BRL"}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
